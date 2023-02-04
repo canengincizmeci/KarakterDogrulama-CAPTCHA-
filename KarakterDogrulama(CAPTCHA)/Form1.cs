6ee5f680cb9f058bf759c42d97807f98cbc6be77
Can Engin Çizmeci <canncizmeci@gmail.com>
2023-02-04 11:36:00 +0300

    Add project files.

---
+++ KarakterDogrulama(CAPTCHA)/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KarakterDogrulama_CAPTCHA_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="can"&& textBox2.Text=="12345"&&textBox3.Text==label1.Text)
+            {
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string karakter1;
+            string[] dizi1 = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "Y", "Z", "W", "X" };
+            int sembol1 = rnd.Next(0, dizi1.Length);
+            karakter1 = dizi1[sembol1];
+            //label1.Text = karakter1;
+
+            string karakter2;
+            string[] dizi2 = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+            int sembol2 = rnd.Next(0, dizi2.Length);
+            karakter2 = dizi2[sembol2];
+            //      label1.Text =karakter1+ karakter2;
+
+            string karakter3;
+            string[] dizi3 = { "!", "#", "+", "-", "*", "/", "&", "|", ">", "<", "?" };
+            int sembol3 = rnd.Next(0, dizi3.Length);
+            karakter3 = dizi3[sembol3];
+            label1.Text = karakter1 + karakter2 + karakter3;
+
+            string karakter4;
+            string[] dizi4 = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "y", "z", "q", "w", "x" };
+            int sembol4 = rnd.Next(0, dizi4.Length);
+            karakter4 = dizi4[sembol4];
+
+            string karakter5;
+            string[] dizi5 = { "a", "A", "B", "1", "2", "0", "*", "#", "+", "-", "H", "M", "m", "d", "u", "!" };
+            int sembol5 = rnd.Next(0, dizi5.Length);
+            karakter5 = dizi5[sembol5];
+            label1.Text = karakter1 + karakter2 + karakter3 + karakter4 + karakter5;
+        }
+    }
+}
---
+++ KarakterDogrulama(CAPTCHA)/Form2.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace KarakterDogrulama_CAPTCHA_
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(201, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(253, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tebrikler Giriş Başarılı";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+    }
+}
